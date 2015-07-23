ab35a69af4accfdff0cb8eba260941cc0b1387d8
Anand Hegde <anand@yourstory.com>
2015-07-23 15:57:03 +0530

    authentication is added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:show,:index]
+  before_action :set_pin, only: [:show,:edit, :update, :destroy ]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /pins
   # GET /pins.json
   def index
@@ -65,6 +66,10 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find_by(id: params[:id])
+    end
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not a authorized user" if @pin.nil? 
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
