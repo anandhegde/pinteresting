6a19a62c31a8e91d3d5a05e25bfa7a611dc40480
Anand Hegde <anand@yourstory.com>
2015-07-23 12:58:29 +0530

    scaffold pins added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723064201) do
+ActiveRecord::Schema.define(version: 20150723072536) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
